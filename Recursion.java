623ea6a3fdcff5a295ddbeba5e4933ca5973a774
Siva <logasivasubramani@gmail.com>
2021-09-16 21:10:35 +0530

    String Subsequence Using Recursion

--- Recursion.java
+++ Recursion.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class Recursion
 {
     public static void main(String args[])
     {
+        /*
        // Storing all Indexes using Recursion...
        int[] arr = {1,6,2,1,1,8,4,1};
        int[] res = presenceindex(arr ,  1 , 0 , 0);
@@ -9,6 +12,15 @@ public class Recursion
        {
             System.out.print(res[i]+" ");
        } 
+       */
+       // Getting all Subsequence of Strings...
+       String inp = "abc";
+       ArrayList<String> subsequenceouptut = subsequence(inp);
+       for(int i =0;i<subsequenceouptut.size();i++)
+       {
+           System.out.print(subsequenceouptut.get(i));
+       }
+
     }
     public static int[] presenceindex( int[] arr , int key  , int vindex , int count)
     {
@@ -28,5 +40,28 @@ public class Recursion
             int output[] = presenceindex(arr , key , vindex+1 , count);
             return output;
         }
+    }
+    public static ArrayList<String> subsequence(String inp)
+    {
+
+        if(inp.length() == 0)
+        {
+            ArrayList<String> result = new ArrayList<String>();
+            result.add(" ");
+            return result;
+        }
+        char ch = inp.charAt(0);
+        String remainingstring  = inp.substring(1);
+
+        ArrayList<String> dummy = subsequence(remainingstring);
+
+        ArrayList<String> result = new ArrayList<>();
+
+        for(int i=0 ;i<dummy.size();i++)
+        {
+                result.add(dummy.get(i));
+                result.add(ch+dummy.get(i));
+        }
+        return result;
     }
 }   
